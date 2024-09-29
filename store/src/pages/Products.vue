0f911cbfd67d822ff96c51361710d6942f710ba5
MhmedRjb <mohamed2elfateh@gmail.com>
2024-09-29 13:41:42 +0300

    add a gallery

--- store/src/pages/Products.vue
+++ store/src/pages/Products.vue
@@ -1,9 +1,7 @@
 <template>
 	<body class="ml-5">
 		<h1 class="text-2xl font-bold text-gray-900">🌞 Our stare store 🌞</h1>
-
 		<h2 class="mt-1 font-semibold text-2xl">Our Product</h2>
-
 		<div class="mt-4 grid grid-cols-5 gap-3" v-if="productList">
 			<productCard v-for="product in productList" :key="product.name" :product="product" />
 		</div>
--- store/src/pages/ProductsDetails.vue
+++ store/src/pages/ProductsDetails.vue
@@ -1,9 +1,56 @@
 <template>
-	<h1>Product Details</h1>
-	<h2>{{ route.params.name }}</h2>
+	<div class="p-5">
+		<div v-if="productDoc">
+			<h1 class="text-3xl font-bold text-gray-900">
+				{{ productDoc.name }}
+			</h1>
+			<div class="sm:flex sm:gap-12">
+				<div class="flex gap-3 flex-col w-fit">
+					<div
+						@click="currentPreviewImageURL = image.image_url"
+						class="border-2 border-gray-600/25 rounded hover:border-black-overlay-200 cursor-pointer"
+						:class="[
+							currentPreviewImageURL === image.image_url
+								? 'border-2 border-gray-700'
+								: 'border-gray-100',
+						]"
+						v-for="image in productDoc.images"
+					>
+						<img class="h-16" :src="image.image_url" alt="dd" />
+					</div>
+				</div>
+				<img class="sm:max-w-lg" :src="currentPreviewImageURL" />
+			</div>
+			<P class="text-3xl font-bold font-sans">
+				{{ formatPrice(productDoc.price, productDoc.currency) }}
+			</P>
+		</div>
+	</div>
 </template>
 <script setup>
 import { useRoute } from 'vue-router'
+import { computed, ref } from 'vue'
+import formatPrice from '../utils'
+import { createDocumentResource } from 'frappe-ui'
 const route = useRoute()
-route.params.name
+const currentPreviewImageURL = ref('')
+
+const productResource = createDocumentResource({
+	doctype: 'Product',
+	name: route.params.name,
+	auto: true,
+	onSuccess(data) {
+		if (data.images.length > 0) {
+			currentPreviewImageURL.value = data.images[0].image_url
+		} else {
+			currentPreviewImageURL.value = data.preview_image
+		}
+	},
+})
+
+const productDoc = computed(() => {
+	if (productResource.doc) {
+		return productResource.doc
+	}
+})
 </script>
